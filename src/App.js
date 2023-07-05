3bc72d6ea45a57cc43216b3995b97ec248ad7a57
just-Muzz <idrisking759@gmail.com>
2023-07-05 19:29:50 +0100

    removed double import statement

--- src/App.js
+++ src/App.js
@@ -13,8 +13,6 @@ import OurCommunity from "./components/OurCommunity/OurCommunity";
 import Testimonials from "./components/Testimonials";
 import Faq from "./components/Faq";
 import About from "./components/About";
-import Services from "./components/Services";
-
 
 function App() {
   return (
