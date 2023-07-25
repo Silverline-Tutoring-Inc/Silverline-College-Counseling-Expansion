1a6b2fc41e7e13b0bf1842d84604d3376ac5512c
Mann Talati <mann.talati@gmail.com>
2023-07-25 13:35:24 -0500

    changed to Silverline Educational Advisory Services

--- frontend/src/components/AboutSection.js
+++ frontend/src/components/AboutSection.js
@@ -16,7 +16,7 @@ const AboutSection = () => {
 						</h2>
 
 						<p data-aos="fade-up" data-aos-delay="200">
-							Silverline Tutoring is an educational 501(c)(3)
+							Silverline Educational Advisory Services is an educational 501(c)(3)
 							nonprofit connecting tutors and students within our
 							organization of over 5000 dedicated members. Founded
 							by college and high school students, this company
