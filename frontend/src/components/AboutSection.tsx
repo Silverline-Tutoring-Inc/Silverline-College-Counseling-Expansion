a71df88b7816e6a83fa32729b2809050a19dd9d6
Krit Dass <dasskrit@gmail.com>
2023-07-26 18:18:16 +0530

    Fixed all type issues

--- frontend/src/components/AboutSection.tsx
+++ frontend/src/components/AboutSection.tsx
@@ -1,11 +1,11 @@
 const AboutSection = () => {
   return (
-    <div class="section section-2">
-      <div class="container">
-        <div class="row align-items-center">
-          <div class="col-lg-6">
-            <span class="subheading" data-aos="fade-up">About Us</span>
-            <h2 class="heading mb-3" data-aos="fade-up" data-aos-delay="100">Support Our Cause</h2>
+    <div className="section section-2">
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="col-lg-6">
+            <span className="subheading" data-aos="fade-up">About Us</span>
+            <h2 className="heading mb-3" data-aos="fade-up" data-aos-delay="100">Support Our Cause</h2>
 
             <p data-aos="fade-up" data-aos-delay="200">Silverline Tutoring is an educational 501(c)(3) nonprofit
               connecting tutors and students within our organization of over 5000 dedicated members. Founded
@@ -15,16 +15,16 @@ const AboutSection = () => {
             <p data-aos="fade-up" data-aos-delay="200">From the depths of math to complex texts in English,
               every student has struggled. We want
               to bridge this divide in education by providing free tutoring to any student.</p>
-            <p class="my-6" data-aos="fade-up" data-aos-delay="300"><a href="about.html"
-              class="btn btn-primary">Read
+            <p className="my-6" data-aos="fade-up" data-aos-delay="300"><a href="about.html"
+              className="btn btn-primary">Read
               more</a></p>
           </div>
-          <div class="col-lg-6 pt-4 px-4">
-            <div class="row">
-              <div class="col-12 col-lg-12" data-aos="fade-up" data-aos-delay="400">
+          <div className="col-lg-6 pt-4 px-4">
+            <div className="row">
+              <div className="col-12 col-lg-12" data-aos="fade-up" data-aos-delay="400">
                 <img src={require("../images/Home/about_1.png")}
                   alt="A tutoring session between a college student from Carnegie Mellon Univeristy and a highschooler"
-                  class="gen-img" />
+                  className="gen-img" />
               </div>
             </div>
           </div>
