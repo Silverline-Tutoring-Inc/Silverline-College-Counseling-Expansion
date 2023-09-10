0d2c24d74ad740cf8f0121c0c9dc50925d48df00
Mann Talati <mann.talati@gmail.com>
2023-09-09 21:58:07 -0500

    changed org name

--- frontend/src/components/OurCommunity/AboutOurCommunity.tsx
+++ frontend/src/components/OurCommunity/AboutOurCommunity.tsx
@@ -23,7 +23,7 @@ const AboutOurCommunity: React.FC = () => {
 					data-aos-delay="100"
 				></h2>
 				<p style={bodyStyle} data-aos="fade-up" data-aos-delay="200">
-				Silverline is filled with students eager to enrich their educational experiences.. Our communication networks promote healthy discourse between students, allowing for members to find others with similar interests. In this sense, students have the opportunity to connect on a higher level than simply access to resources and advisory. They can discuss sports, school, musical interests and any hobbies they have in mind. Additionally, our community is divided into specific categories of interest. Silverline instills a stronger sense of community between members that facilitates a stress-free and positive learning environment for all involved.
+				Silverline is filled with students eager to enrich their educational experiences. Our communication networks promote healthy discourse between students, allowing for members to find others with similar interests. In this sense, students have the opportunity to connect on a higher level than simply access to resources and advisory. They can discuss sports, school, musical interests and any hobbies they have in mind. Additionally, our community is divided into specific categories of interest. Silverline instills a stronger sense of community between members that facilitates a stress-free and positive learning environment for all involved.
 				</p>
 				<p style={bodyStyle} data-aos="fade-up" data-aos-delay="300">
 				We understand that high school can be stressful, so we provide a plethora of resources for students in our community to take a breather and relax. Many of these contests often have prizes, providing an incentive for all students.
